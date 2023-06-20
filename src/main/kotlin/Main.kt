47ede53930ab0eeb95c92ded4a18d1f9a8bd357f
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-06-20 09:03:33 +0100

    Strings mit + konkatenieren (zusammensetzen)
    Einen zusammengesetzten String in einer Variable speichern

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,6 +10,20 @@ fun main() {
 
     // Konstante, in SNAKE_CASE, DAYS_OF_THE_WEEK, DAY_OF_BIRTH, etc.
     val LAST_NAME = "Weasley"
-    LAST_NAME = "Potter"
+    // LAST_NAME = "Potter"
+    println(LAST_NAME)
+
+    val PI = 3.14
+    val WEEK_DAYS = 7
+
+    println("Zusammengesetzter Name: " + firstName+ " " + LAST_NAME)
+
+    // variable, die aus mehreren anderen zusammengesetzten Variablen besteht:
+    var fullName = firstName + " " + LAST_NAME
+    println("fullName Variable: $fullName")
+
+
+
+
 
 }
