4183749819a91759988d9b6ea43e73654b561999
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-06-20 08:59:57 +0100

    Variable initialisieren und aktualisieren

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    // Initialisierung: Variable, camelCase: fullName, firstAndLastName, isUserLoggedIn, etc.
+    var firstName = "Harry"
+    println("Alter Name bei Initialisierung: $firstName")
+
+    // Aktualisierung:
+    firstName = "Ron"
+    println("Neuer Name bei Aktualisierung: $firstName")
+
+
+
+}
